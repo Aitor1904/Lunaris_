5557a58d548c34c9d878a9937ec80802d9daecca
Ander_Clase <aitorlopezolivares@gmail.com>
2024-12-05 08:32:19 +0100

    Movement/Attack

---
+++ Assets/Proyect/Scripts/Player/Movement.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [Header("BasicMovement")]
+    Rigidbody rb;
+    [SerializeField]
+    float speed;
+    float horizontalMovement;
+    bool isLookingRight = true;
+    [Header("Jump")]
+    [SerializeField]
+    float jumpForce;
+    bool isJumping = false;
+    [Header("Dash")]
+    [SerializeField]
+    private float dashingTime = 0.2f;
+    [SerializeField]
+    private float dashForce = 20f;
+    [SerializeField]
+    private float timeCanDash = 1f;
+    private bool isDashing;
+    private bool canDash = true;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Awake()
+    {
+
+    }
+    private void Update()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+        
+        rb.linearVelocity = new Vector3(horizontalMovement * speed, rb.linearVelocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            horizontalMovement = context.ReadValue<Vector2>().x;
+
+        }
+        if (context.canceled)
+        {
+            horizontalMovement = 0;
+        }
+        if (horizontalMovement < 0 && isLookingRight)
+        {
+            Flip();
+        }
+        if (horizontalMovement > 0 && !isLookingRight)
+        {
+            Flip();
+        }
+    }
+    public void ActivateDash(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    private int IsLookingRightToInt(bool isLookingRight)
+    {
+        if (isLookingRight)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false;
+        rb.useGravity = false;
+        Debug.Log(IsLookingRightToInt(isLookingRight) * dashForce);
+        rb.linearVelocity = new Vector2(IsLookingRightToInt(isLookingRight) * dashForce, 0f);
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        rb.useGravity = true;
+        yield return new WaitForSeconds(timeCanDash);
+        canDash = true;
+        Debug.Log("Hace");
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+    }
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        isLookingRight = !isLookingRight;
+    }
+
+}
